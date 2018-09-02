a0eda39560f7fea53b2af465cf96dd279ae30d74
KGD <keithgdowney@gmail.com>
2018-09-01 22:55:16 -0700

    something works! the buttons do

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,88 +1,88 @@
-
-    //Array for searched reactions to be added
-    var emotions = [];
-    
-        //Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
-      //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
-         function displayReactions() {
-    
-        var x = $(this).data("search");
-        console.log(x);
-    
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=x4MZ9O1HbmLmrigfDuc10vUMwH0XJJmiC&limit=10";
-    
-        console.log(queryURL);
-    
-        $.ajax({
-              url: queryURL,
-              method: "GET"
-            }).done(function(response) {
-                var results = response.data;
-                console.log(results);
-                for (var i = 0; i < results.length; i++) {
-                
-                var reactionDiv = $("<div class='col-md-4'>");
-    
-                var rating = results[i].rating;
-                var defaultAnimatedSrc = results[i].images.fixed_height.url;
-                var staticSrc = results[i].images.fixed_height_still.url;
-                var reactionImage = $("<img>");
-                var p = $("<p>").text("Rating: " + rating);
-    
-                reactionImage.attr("src", staticSrc);
-                reactionImage.addClass("reactionGiphy");
-                reactionImage.attr("data-state", "still");
-                reactionImage.attr("data-still", staticSrc);
-                reactionImage.attr("data-animate", defaultAnimatedSrc);
-                reactionDiv.append(p);
-                reactionDiv.append(reactionImage);
-                $("#reactionArea").prepend(reactionDiv);
-    
-            }
+$(document).ready(function() {
+  //Array for searched topics to be added
+  var topics = [];
+  
+    //Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
+    //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
+     function displayReactions() {
+  
+    var x = $(this).data("search");
+    console.log(x);
+  
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=x4MZ9O1HbmLmrigfDuc10vUMwH0XJJmiC&limit=10";
+  
+    console.log(queryURL);
+  
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).done(function(response) {
+            var results = response.data;
+            console.log(results);
+            for (var i = 0; i < results.length; i++) {
+            
+            var showDiv = $("<div class='col-md-4'>");
+  
+            var rating = results[i].rating;
+            var defaultAnimatedSrc = results[i].images.fixed_height.url;
+            var staticSrc = results[i].images.fixed_height_still.url;
+            var showImage = $("<img>");
+            var p = $("<p>").text("Rating: " + rating);
+  
+            showImage.attr("src", staticSrc);
+            showImage.addClass("reactionGiphy");
+            showImage.attr("data-state", "still");
+            showImage.attr("data-still", staticSrc);
+            showImage.attr("data-animate", defaultAnimatedSrc);
+            showDiv.append(p);
+            showDiv.append(showImage);
+            $("#reactionArea").prepend(showDiv);
+  
+          }
+    });
+  }
+  
+    //Submit button click event takes search term from form input, trims and pushes to topics array, displays button
+    $("#addReaction").on("click", function(event) {
+          event.preventDefault();
+          var newShow = $("#reactionInput").val().trim();
+          topics.push(newShow);
+          console.log(topics);
+          $("#reactionInput").val('');
+          displayButtons();
         });
-    }
-    
-      //Submit button click event takes search term from form input, trims and pushes to emotions array, displays button
-   
-        $("#addReaction").on("click", function(event) {
-            event.preventDefault();
-            var newReaction = $("#reactionInput").val().trim();
-            emotions.push(newReaction);
-            console.log(emotions);
-            $("#reactionInput").val('');
-            displayButtons();
-          });
-    
-      //Function iterates through emotions array to display button with array values in "reactionButtons" section of HTML
-        function displayButtons() {
-        $("#reactionButtons").empty();
-        for (var i = 0; i < emotions.length; i++) {
-          var a = $('<button class="btn btn-secondary">');
-          a.attr("id", "reaction");
-          a.attr("data-search", emotions[i]);
-          a.text(emotions[i]);
-          $("#reactionButtons").append(a);
-        }
-      }
-    
-    
-      displayButtons();
-    
-      //Click event on button with id of "reaction" executes displayReactions function
-      $(document).on("click", "#reaction", displayReactions);
-      
-      //Click event on gifs with class of "reactionGiphy" executes pausePlayGifs function
-      $(document).on("click", ".reactionGiphy", pausePlayGifs);
-    
-      //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
-      function pausePlayGifs() {
-           var state = $(this).attr("data-state");
-          if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
+  
+    //Function iterates through topics array to display button with array values in "myButtons" section of HTML
+    function displayButtons() {
+      $("#myButtons").empty();
+      for (var i = 0; i < topics.length; i++) {
+        var a = $('<button class="btn btn-primary">');
+        a.attr("id", "show");
+        a.attr("data-search", topics[i]);
+        a.text(topics[i]);
+        $("#myButtons").append(a);
       }
     }
-    
+  
+  
+    displayButtons();
+  
+    //Click event on button with id of "show" executes displayNetflixShow function
+    $(document).on("click", "#theReaction", displayReactions);
+  
+    //Click event on gifs with class of "netflixGiphy" executes pausePlayGifs function
+    $(document).on("click", ".reactionGiphy", pausePlayGifs);
+  
+    //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
+    function pausePlayGifs() {
+       var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+    }
+  }
+  
+  });
